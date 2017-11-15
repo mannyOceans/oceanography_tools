dca6806fb3a97a406a776c1d5e4157e02e230b25
Manish Devana <md4n14@soton.ac.uk>
2017-11-15 10:42:56 +0000

    Add files via upload

---
+++ ray_tracing_toolbox.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 15 10:40:35 2017
+
+
+Tools for running ray racing on internal waves in a general sense
+
+
+
+@author: manishdevana
+"""
+
